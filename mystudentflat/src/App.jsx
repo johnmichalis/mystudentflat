4dd32567cbbbab631dd2bb3831625b6515104f07
johnmichalis <john@johnmichalis.com>
2026-04-02 03:25:14 +0300

    Add files via upload

---
+++ mystudentflat/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+export default function App() {
+  return (
+    <div style={{padding:40,fontFamily:"sans-serif",textAlign:"center"}}>
+      <h1>MyStudentFlat - Test Build</h1>
+      <p>If you see this, the deploy pipeline works. The issue is in the app code.</p>
+    </div>
+  );
+}
